1f6069915cba0b132944d6c8665f8c2fd00c2abb
ohmlim1234567@gmail.com <ohmlim1234567@gmail.com>
2020-09-15 11:50:44 +0700

    Add project files.

---
+++ 3program---week6(2/Source.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int N;
+	scanf_s("%d",&N);
+	for (int i = 1;i <= N;i++)
+	{
+		for (int j = 1;j <= N;j++)
+		{
+			if (i == j || i < j)
+			{
+				printf("*");
+			}
+			else
+			{
+				printf(" ");
+			}
+			
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
